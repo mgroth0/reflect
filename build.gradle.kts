33fc6d5e6dbcfcb02c9a9b30b62c863277e52499
mgroth0 <mgroth49@gmail.com>
2022-07-25 21:46:13 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,11 +24,4 @@ dependencies {
   api(libs.kt.reflect)
 //  api(projects.k.kbuild)
   /*api(projects.klib)*/
-//  if (rootDir.name.upper() == "FLOW") {
-//	api(project(":k:klib")) {
-//	  targetConfiguration = "jvmRuntimeElements"
-//	}
-//  } else {
-//	api("matt.k:klib:+")
-//  }
 }
