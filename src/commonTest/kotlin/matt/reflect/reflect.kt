9141ca7c262408201aa973f9944797523f0058dd
mgroth0 <mgroth49@gmail.com>
2022-10-26 19:44:44 -0400

    autocommit

--- src/commonTest/kotlin/matt/reflect/reflect.kt
+++ src/commonTest/kotlin/matt/reflect/reflect.kt
@@ -2,33 +2,4 @@ package matt.reflect
 
 import kotlin.contracts.ExperimentalContracts
 
-class SomeTests {
-
-  @Test
-  @ExperimentalContracts
-  fun testClassForName() = reportAndReThrowErrors {
-	yesIUseTestLibs()
-	assertEquals(String::class, classForName("kotlin.String"))
-	assertEquals(Int::class, classForName("kotlin.Int"))
-	assertEquals(Runtime::class, classForName("java.lang.Runtime"))
-  }
-
-
-  /*https://stackoverflow.com/questions/30445974/kotlin-java-lang-nosuchmethoderror-in-tests*/
-  fun reportAndReThrowErrors(op: ()->Unit) {
-	try {
-	  op()
-	} catch (throwable: Throwable) {
-	  var e: Throwable? = throwable
-	  do {
-		e!!
-		println(e)
-		e.printStackTrace()
-		e = e.cause
-	  } while (e != null)
-	  throw throwable
-	}
-
-  }
-
-}
+expect class SomeTests
