7de75d0afdf054878cfb2c65a9202f6ee4d640e1
mgroth0 <mgroth49@gmail.com>
2023-03-27 21:51:02 -0400

    autocommit

--- src/jsTest/kotlin/matt/reflect/reflect.kt
+++ src/jsTest/kotlin/matt/reflect/reflect.kt
@@ -4,8 +4,8 @@ import matt.test.reportAndReThrowErrors
 import kotlin.test.Test
 
 actual class ReflectTests {
-  @Test
-  actual fun testClassForName() = reportAndReThrowErrors {
-	/*It is not implemented in javascript yet*/
-  }
+    @Test
+    actual fun testClassForName() = reportAndReThrowErrors {
+        /*It is not implemented in javascript yet*/
+    }
 }
