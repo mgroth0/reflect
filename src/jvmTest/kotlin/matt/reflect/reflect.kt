2e1c7c654254af40013612243da8bf9b1cf952df
mgroth0 <mgroth49@gmail.com>
2023-04-14 22:34:06 -0400

    changed src/jvmTest/kotlin/matt/reflect/reflect.kt

--- src/jvmTest/kotlin/matt/reflect/reflect.kt
+++ src/jvmTest/kotlin/matt/reflect/reflect.kt
@@ -10,15 +10,15 @@ import kotlin.test.assertEquals
 
 actual class ReflectTests {
 
-  @Test
-  @ExperimentalContracts
-  actual fun testClassForName() = reportAndReThrowErrors {
-	yesIUseTestLibs()
-	assertEquals(String::class, classForName("kotlin.String"))
-	assertEquals(String::class, classForName("kotlin.String?")) /*for kotlinx.serialization*/
-	assertEquals(Int::class, classForName("kotlin.Int"))
-	assertEquals(Runtime::class, classForName("java.lang.Runtime"))
-  }
+    @Test
+    @ExperimentalContracts
+    actual fun testClassForName() = reportAndReThrowErrors {
+        yesIUseTestLibs()
+        assertEquals(String::class, classForName("kotlin.String"))
+        assertEquals(String::class, classForName("kotlin.String?")) /*for kotlinx.serialization*/
+        assertEquals(Int::class, classForName("kotlin.Int"))
+        assertEquals(Runtime::class, classForName("java.lang.Runtime"))
+    }
 
 
 }
