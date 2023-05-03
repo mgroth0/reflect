a94b2142aad6f1a9e16637f15cc1aadcfd345d4b
mgroth0 <mgroth49@gmail.com>
2023-05-03 12:57:30 -0400

    changed src/jvmMain/kotlin/matt/reflect/reflections/reflections.kt

--- src/jvmMain/kotlin/matt/reflect/reflections/reflections.kt
+++ src/jvmMain/kotlin/matt/reflect/reflections/reflections.kt
@@ -1,10 +1,12 @@
 package matt.reflect.reflections
 
 import matt.lang.RUNTIME
+import matt.lang.anno.oexp.PyClass
 import org.reflections8.Reflections
 import org.reflections8.scanners.MethodAnnotationsScanner
 import org.reflections8.util.ConfigurationBuilder
 import java.lang.reflect.Method
+import java.net.URL
 import kotlin.reflect.KClass
 import kotlin.reflect.jvm.kotlinFunction
 
@@ -13,23 +15,25 @@ fun KClass<out Annotation>.annotatedMattKTypes(): List<KClass<out Any>> = annota
 fun KClass<out Annotation>.annotatedMattKFunctions() = annotatedMattJFunctions().map { it.kotlinFunction }
 
 fun KClass<out Annotation>.annotatedMattJFunctions(): Set<Method> =
-  methodScanningMattConfig.reflection().getMethodsWithAnnotation(this)!!
+    methodScanningMattConfig.reflection().getMethodsWithAnnotation(this)!!
 
 
 fun KClass<out Annotation>.annotatedMattJTypes(): Set<Class<*>> = defaultMattConfig
-  .reflection()
-  .getTypesAnnotationWith(this)
+    .reflection()
+    .getTypesAnnotationWith(this)
 
-@Synchronized fun <T: Any> KClass<T>.mattSubClasses() = subclasses(MATT_PACK)
+@Synchronized
+fun <T : Any> KClass<T>.mattSubClasses() = subclasses(MATT_PACK)
 
-@Synchronized fun <T: Any> KClass<T>.subclasses(pack: String): List<KClass<out T>> {
-  val searchParams = SubClassSearchParams(this, pack)
-  @Suppress("UNCHECKED_CAST") return (subclassCache[searchParams] ?: run {
-	val cfg = ReflectionConfig(pack)
-	val subClasses = cfg.reflection().getSubTypesOf(this)
-	subclassCache[searchParams] = subClasses
-	subClasses
-  }) as List<KClass<out T>>
+@Synchronized
+fun <T : Any> KClass<T>.subclasses(pack: String): List<KClass<out T>> {
+    val searchParams = SubClassSearchParams(this, pack)
+    @Suppress("UNCHECKED_CAST") return (subclassCache[searchParams] ?: run {
+        val cfg = ReflectionConfig(pack)
+        val subClasses = cfg.reflection().getSubTypesOf(this)
+        subclassCache[searchParams] = subClasses
+        subClasses
+    }) as List<KClass<out T>>
 }
 
 
@@ -37,51 +41,51 @@ private val defaultMattConfig = ReflectionConfig()
 private val methodScanningMattConfig = ReflectionConfig(scanMethodAnnotations = true)
 
 private data class ReflectionConfig(
-  val pack: String = MATT_PACK,
-  val scanMethodAnnotations: Boolean = false
+    val pack: String = MATT_PACK,
+    val scanMethodAnnotations: Boolean = false
 ) {
-  fun reflection() = synchronized(reflectionsCache) {
-	reflectionsCache[this] ?: Reflection(this).also {
-	  reflectionsCache[this] = it
-	}
-  }
+    fun reflection() = synchronized(reflectionsCache) {
+        reflectionsCache[this] ?: Reflection(this).also {
+            reflectionsCache[this] = it
+        }
+    }
 }
 
 private val reflectionsCache = mutableMapOf<ReflectionConfig, Reflection>()
 
 private class Reflection(
-  cfg: ReflectionConfig,
+    cfg: ReflectionConfig,
 ) {
-  private val reflections by lazy {
-	/*val t = System.nanoTime()
-	debug("getting Reflections...")*/
+    private val reflections by lazy {
+        /*val t = System.nanoTime()
+        debug("getting Reflections...")*/
 
-	var config = ConfigurationBuilder().useParallelExecutor(RUNTIME.availableProcessors())
-	if (cfg.scanMethodAnnotations) {
-	  config = config.addScanners(MethodAnnotationsScanner())
-	}
-
-
-	config = config.forPackages(cfg.pack)
-
-	val r = Reflections(config)
+        var config = ConfigurationBuilder().useParallelExecutor(RUNTIME.availableProcessors())
+        if (cfg.scanMethodAnnotations) {
+            config = config.addScanners(MethodAnnotationsScanner())
+        }
 
 
-	/*val tt = System.nanoTime()
-	val d = Duration.ofNanos(tt - t).toMillis()
-	debug("getting Reflections took $d ms")*/
-	r
-  }
+        config = config.forPackages(cfg.pack)
 
-  fun getSubTypesOf(kClass: KClass<*>) = reflections.getSubTypesOf(kClass.java).map { it.kotlin }
+        val r = Reflections(config)
 
-  fun getMethodsWithAnnotation(cls: KClass<out Annotation>) = reflections.getMethodsAnnotatedWith(
-	cls.java
-  )
 
-  fun getTypesAnnotationWith(cls: KClass<out Annotation>) = reflections.getTypesAnnotatedWith(
-	cls.java
-  )
+        /*val tt = System.nanoTime()
+        val d = Duration.ofNanos(tt - t).toMillis()
+        debug("getting Reflections took $d ms")*/
+        r
+    }
+
+    fun getSubTypesOf(kClass: KClass<*>) = reflections.getSubTypesOf(kClass.java).map { it.kotlin }
+
+    fun getMethodsWithAnnotation(cls: KClass<out Annotation>) = reflections.getMethodsAnnotatedWith(
+        cls.java
+    )
+
+    fun getTypesAnnotationWith(cls: KClass<out Annotation>) = reflections.getTypesAnnotatedWith(
+        cls.java
+    )
 
 }
 
@@ -90,3 +94,21 @@ data class SubClassSearchParams(val cls: KClass<*>, val pack: String = MATT_PACK
 private val subclassCache = mutableMapOf<SubClassSearchParams, List<KClass<*>>>()
 
 private const val MATT_PACK = "matt"
+
+
+fun playWithReflections(jarURL: URL): String {
+    val cfg = ConfigurationBuilder()
+        /*.addUrls()
+        .addClassLoader()
+        .addClassLoaders()
+        .forPackages()*/
+        .setUrls(jarURL)
+
+    Reflections(cfg)
+        .getTypesAnnotatedWith(PyClass::class.java)
+
+
+    return "todo"
+
+
+}
